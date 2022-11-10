7093f98e86e3c32fe286e462b18cff2f5b6a4bf0
weihong <1745627971@qq.com>
2022-11-10 18:44:45 +0800

    完成了搜索功能的开发

--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -1,5 +1,7 @@
 <template>
 	<view>
+		<!-- 使用自定义搜索组件 -->
+		<my-search @click='gotoSearch'></my-search>
 		<view class="scroll-view-container">
 			<!-- 左侧滑动区域 -->
 			<scroll-view class="left-scroll-view" scroll-y="true" :style="{height:wh+'px'}">
@@ -26,7 +28,7 @@
 </template>
 
 <script>
-	export default {
+	export default { 	
 		data() {
 			return {
 				//当前设备可用高度
@@ -43,7 +45,7 @@
 		},
 		onLoad() {
 			const sysInfo = uni.getSystemInfoSync().windowHeight
-			this.wh = sysInfo
+			this.wh = sysInfo-50
 			this.getCateList()
 		},
 		methods: {
@@ -64,6 +66,11 @@
 			gotoGoodsList(item){
 				uni.navigateTo({
 					url:'/subpkg/goods_list/goods_list?cid='+item.cat_id
+				})
+			},
+			gotoSearch(){
+				uni.navigateTo({
+					url:'/subpkg/search/search'
 				})
 			}
 		}
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,5 +1,8 @@
 <template>
 	<view>
+		<view @click="gotoSearch" class="search-box">
+			<my-search></my-search>
+		</view>
 		<!-- 轮播图区域 -->
 		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
 			<swiper-item v-for="(item,i) in swiperList" :key="i">
@@ -96,6 +99,11 @@
 					})
 				})
 				this.floorList = res.message
+			},
+			gotoSearch(){
+				uni.navigateTo({
+					url:'/subpkg/search/search'
+				})
 			}
 		}
 	}
@@ -137,5 +145,10 @@
 	.floor-img-box{
 		display: flex;
 		padding-left: 10rpx;
+	}
+	.search-box{
+		position: sticky;
+		top: 0;
+		z-index: 999;
 	}
 </style>
